867b34f074a76c8dd6aad315e1c11365596bf711
jhasunny07 <sunnyjha184@gmail.com>
2026-01-24 22:18:15 +0530

    Add ISR

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,13 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: "2024-01-01",
+
+  // 🚨 THIS IS CRITICAL
+  useCdn: false,
+
+  // ensures no build-time caching
+  perspective: "published",
+});
